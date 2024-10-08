2440ae3d3bde9f1957c2c73b97570d9bb1bf48e6
Luis Valladolid <20213tn002@utez.edu.mx>
2024-10-08 16:08:37 -0600

    [ADD] Avance #4
    Clase DMI 08/10/2024

--- lib/modules/auth/login.dart
+++ lib/modules/auth/login.dart
@@ -1,57 +1,74 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  bool _isObscure = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        backgroundColor: const Color.fromARGB(255, 26, 207, 180),
-      ),
-      body: Column(
-        children: [
-          const Row(
-            children: [
-              Padding(
-                padding:
-                    EdgeInsets.all(16.0),
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage('assets/usuario.png'),
-                      radius: 20,
-                    ),
+        appBar: AppBar(
+          title: const Text('Login'),
+          backgroundColor: const Color.fromARGB(255, 26, 207, 180),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Image.asset('assets/stop.png', width: 200, height: 200),
+            const SizedBox(height: 16),
+            TextFormField(
+              decoration: const InputDecoration(
+                //icon: Icon(Icons.person),
+                hintText: 'Correo Electrónico',
+                label: Text('Correo Electrónico'),
+                labelStyle: TextStyle(color: Colors.black),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Luis Valladolid', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.0),),
-                  Text('Hace 10 minutos', style: TextStyle(color: Colors.black54, fontSize: 12),)
-                ],
-              ),
-              Expanded(child: Align(alignment: Alignment.centerRight, child: Padding(padding: EdgeInsets.all(16.0), child: Icon(Icons.more_vert)))),
-            ],
-          ),
-          Image.asset('assets/init.jfif',  width: double.infinity, height: 200),
-          const Row(
-            children: [
-              Padding(padding: EdgeInsets.all(16.0), child: Icon(Icons.favorite, color: Colors.red)),
-              Padding(padding: EdgeInsets.all(16.0), child: Icon(Icons.message),),
-              Padding(padding: EdgeInsets.all(16.0), child: Icon(Icons.send),),
-              Expanded(child: Align(alignment: Alignment.centerRight, child: Padding(padding: EdgeInsets.all( 16.0), child: Icon(Icons.save)))),
-            ],
-          ),
-           Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/menu');
-              },
-              child: const Text('Go back!'),
+              keyboardType: TextInputType.emailAddress,
+              controller: _email,
             ),
-          ),
-        ],
-      ),
-      
-    );
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  label: const Text('Contraseña'),
+                  labelStyle: const TextStyle(color: Colors.black),
+                  icon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                      icon: Icon(_isObscure
+                          ? Icons.visibility
+                          : Icons.visibility_off))),
+              controller: _password,
+              obscureText: _isObscure,
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Email: ${_email.text}');
+                    print('Password: ${_password.text}');
+                  },
+                  style: OutlinedButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      )),
+                  child: const Text('Iniciar Sesión'),
+                ))
+          ]),
+        ));
   }
 }
