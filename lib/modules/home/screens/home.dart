c77b2e93633fb5d561dd016e704717ec82c00957
Kellguzdi <20213tn002@utez.edu.mx>
2024-10-23 18:58:39 -0600

    [NEW] Firestore 23/10
    Hay que arreglarlo xd

--- lib/modules/home/screens/home.dart
+++ lib/modules/home/screens/home.dart
@@ -1,8 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:learning_lebc_9c/modules/home/screens/content_column.dart';
+//import 'package:learning_lebc_9c/modules/home/screens/content_column.dart';
 
-class Home extends StatelessWidget {
+class Restaurant {
+  final String _titulo;
+  final String _description;
+  final List<String>_images;
+  final double _rating;
+  final int _count;
+
+  Restaurant(this._titulo, this._description, this._images, this._rating, this._count);
+
+}
+
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final db =  FirebaseFirestore.instance;
+  final List<Restaurant> _restaurants = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    (() async => {
+      await db.collection("restaurantes").get().then((event) => {
+       for(var doc in event.docs){
+        //print('${doc.id} => ${doc.data()}')
+        /*final restaurants = Restaurant(
+          titulo: doc['titulo'],
+          description: doc['description'],
+          images: doc['images'],
+          rating: doc['rating'],
+          price: doc['price'],
+        )*/
+        final restaurant = Restaurant(
+          doc.data()['titulo'],
+          doc.data()['description'],
+          List<String>.from(doc.data()['images']),
+          doc.data()['rating'],
+          doc.data()['price'],
+        );
+        _restaurants.add(restaurant);
+       }   
+      });
+    })();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,24 +66,16 @@ class Home extends StatelessWidget {
         child: const Icon(Icons.home),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Row(
-          children: [
-            ContentColumn(
-              title: 'Bienvenido',
-              description: 'a mi aplicación',
-            ),
-            ContentColumn(
-              title: 'Otra Línea',
-              description: 'Segundo Renglón',
-            ),
-            ContentColumn(
-              title: 'Otra Línea',
-              description: 'Tercer Renglón',
-            ),
-          ],
-        ),
+      body: const Row(
+        children: [
+          //Image.network(restaurants[0].images[0], width: 150, height: 150,),
+          const SizedBox(width: 8,),
+          Column(
+            children: [
+              //Text(restaurants[0].name, style: const TextStyle(fontSize: 14.0, color: Colors.black),)
+            ],
+          )
+        ],
       ),
     );
   }
